d84d46e5608784af8e7db1ff982e607c3742beb6
Manel Matougui <m.matougui@gmail.com>
2018-10-23 22:17:50 -0700

    add Initial viewController

--- TimeTracker/TimeTracker/ProjectViewController.swift
+++ TimeTracker/TimeTracker/ProjectViewController.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+import UIKit
+class ProjectViewController : UIViewController {
+    
+    @IBOutlet weak var navBar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //navBar.topItem?.title = "hello"
+    }
+   
+    
+}
