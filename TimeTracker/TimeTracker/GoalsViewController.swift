42476f3f34a93901451a0db91c642188aae9f5ef
Manel Matougui <m.matougui@gmail.com>
2018-10-23 22:43:24 -0700

    add Project View Controller

---
+++ TimeTracker/TimeTracker/GoalsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GoalsViewController.swift
+//  TimeTracker
+//
+//  Created by Manel matougui on 10/23/18.
+//  Copyright © 2018 udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GoalsViewController : UIViewController {
+    
+}
