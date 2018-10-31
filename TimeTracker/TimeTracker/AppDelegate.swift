7768ba732a82e7bc45b51e098e1ee6272fcdc1b2
Manel Matougui <m.matougui@gmail.com>
2018-10-30 21:51:00 -0700

    set up the stack in TimeTracker

--- TimeTracker/TimeTracker/AppDelegate.swift
+++ TimeTracker/TimeTracker/AppDelegate.swift
@@ -13,9 +13,11 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let dataController = DataController(modelName: "TimeTracker")
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        dataController.load()
         // Override point for customization after application launch.
         return true
     }
---
+++ TimeTracker/TimeTracker/Model/DataController.swift
@@ -0,0 +1,33 @@
+//
+//  DataController.swift
+//  TimeTracker
+//
+//  Created by Manel matougui on 10/30/18.
+//  Copyright © 2018 udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+class DataController {
+    
+    let persistentContainer :NSPersistentContainer
+    var viewContext: NSManagedObjectContext{
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load (completion: (() -> Void )? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, Error in
+            guard Error == nil else {
+                fatalError(Error!.localizedDescription)
+            }
+        }
+        completion?()
+    }
+    
+    
+    
+}
