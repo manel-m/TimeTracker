8a4690e5236fe554b10b02a4bf8f4c6ef8e5a687
Manel Matougui <m.matougui@gmail.com>
2018-10-30 21:27:50 -0700

    add timer

--- TimeTracker/TimeTracker/ViewControllers/ProjectViewController.swift
+++ TimeTracker/TimeTracker/ViewControllers/ProjectViewController.swift
@@ -11,14 +11,37 @@ import UIKit
 class ProjectViewController : UIViewController {
     
     var projectName : String! = ""
+    var time = 0
+    // timer
+    var timer = Timer()
+    
     
     @IBOutlet weak var navBar: UINavigationItem!
-    
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         navBar.title = projectName
         print(projectName)
     }
-   
     
+    
+    @IBAction func StartTimer(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ProjectViewController.Action), userInfo: nil, repeats: true)
+        //scheduledTimer
+    }
+    
+    
+    @IBAction func PauseTimer(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    @IBAction func StopTimer(_ sender: UIButton) {
+        timer.invalidate()
+        time = 0
+        timeLabel.text = "0"
+    }
+    
+    @objc func Action (){
+        time += 1
+        timeLabel.text = String(time)
+    }
 }
