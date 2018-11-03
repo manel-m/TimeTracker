8b28cf6f07d5f50c0e5781a1ad1fe7e0bfa88ae7
Manel Matougui <m.matougui@gmail.com>
2018-11-02 21:12:04 -0700

    add ProjectsStatViewController and GoalsStatViewController

--- TimeTracker/TimeTracker/ViewControllers/GoalsStatViewController.swift
+++ TimeTracker/TimeTracker/ViewControllers/GoalsStatViewController.swift
@@ -10,4 +10,23 @@ import Foundation
 import  UIKit
 class GoalsStatViewController: UITableViewController {
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell")!
+        //        let villain = self.allVillains[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        //        cell.textLabel?.text = villain.name
+        //        cell.imageView?.image = UIImage(named: villain.imageName)
+        
+        // If the cell has a detail label, we will put the evil scheme in.
+        //        if let detailTextLabel = cell.detailTextLabel {
+        //            detailTextLabel.text = "Scheme: \(villain.evilScheme)"
+        
+        return cell
+    }
 }
+
